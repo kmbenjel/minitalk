9e3e44f8a9b7fe1f6b3bdb0cfc70a0932944b344
Khalid Benjelloun <khalidmailing@gmail.com>
2023-05-30 06:35:57 +0100

    kbenjell

--- pg/client.c
+++ pg/client.c
@@ -3,6 +3,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netdb.h>
 #include <unistd.h>
 #include <string.h>
 
@@ -14,71 +15,54 @@ void error(const char *msg) {
     exit(1);
 }
 
-void handle_client() {
-    int newsockfd, clilen, n;
-    char buffer[BUFFER_SIZE];
-    struct sockaddr_in cli_addr;
-
-    clilen = sizeof(cli_addr);
-
-    // Accept connection
-    newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, (socklen_t *)&clilen);
-    if (newsockfd < 0)
-        error("ERROR on accept");
-
-    // Receive message from client
-    memset(buffer, 0, BUFFER_SIZE);
-    n = read(newsockfd, buffer, BUFFER_SIZE - 1);
-    if (n < 0)
-        error("ERROR reading from socket");
-
-    // Print received message
-    write(STDOUT_FILENO, "Received message: ", strlen("Received message: "));
-    write(STDOUT_FILENO, buffer, strlen(buffer));
-    write(STDOUT_FILENO, "\n", 1);
-
-    // Close socket
-    close(newsockfd);
-}
-
-int main() {
-    int portno;
+void send_message(const char *hostname, int portno, const char *message) {
     struct sockaddr_in serv_addr;
+    struct hostent *server;
 
     // Create socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
         error("ERROR opening socket");
 
+    // Get server host by name
+    server = gethostbyname(hostname);
+    if (server == NULL) {
+        write(STDERR_FILENO, "ERROR, no such host\n", strlen("ERROR, no such host\n"));
+        exit(1);
+    }
+
     // Clear address structure
     memset((char *)&serv_addr, 0, sizeof(serv_addr));
 
-    // Port number
-    portno = 12345;
-
     // Set up the server address structure
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
     serv_addr.sin_port = htons(portno);
 
-    // Bind socket to address
-    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-        error("ERROR on binding");
+    // Connect to server
+    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+        error("ERROR connecting");
 
-    // Listen for connections
-    listen(sockfd, 5);
-    char pid_buffer[32]; // Increased buffer size
-    int pid = getpid();
-    sprintf(pid_buffer, "Server started. PID: %d\n", pid);
-    write(STDOUT_FILENO, pid_buffer, strlen(pid_buffer));
-
-    // Handle clients
-    while (1) {
-        handle_client();
-    }
+    // Send message to server
+    int n = write(sockfd, message, strlen(message));
+    if (n < 0)
+        error("ERROR writing to socket");
 
     // Close socket
     close(sockfd);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        fprintf(stderr, "Usage: %s <hostname> <message>\n", argv[0]);
+        exit(1);
+    }
+
+    const char *hostname = argv[1];
+    const char *message = argv[2];
+    int portno = 12345;
+
+    send_message(hostname, portno, message);
 
     return 0;
 }
