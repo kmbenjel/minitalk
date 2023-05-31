20ca7a722747d57746674bc2df7051fb48532081
Khalid Benjelloun <khalidmailing@gmail.com>
2023-05-31 18:18:47 +0100

    kbenjell

---
+++ pg/minitalk_mlanca-c-2/minitalk.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/28 11:35:11 by mlanca-c          #+#    #+#             */
+/*   Updated: 2021/07/06 13:10:56 by mlanca-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <signal.h>
+# include "libft.h"
+
+/*
+** Colors
+*/
+# define ANSI_COLOR_RED		"\x1b[31m"
+# define ANSI_COLOR_BLUE	"\x1b[34m"
+# define ANSI_COLOR_GREEN	"\x1b[32m"
+# define ANSI_COLOR_YELLOW	"\x1b[33m"
+# define ANSI_COLOR_RESET	"\x1b[0m"
+
+#endif
